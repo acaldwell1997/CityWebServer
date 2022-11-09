14aa37e4cc5d74b9a95f7ffcf6ce053de6c49e90
Alexis Caldwell <46656008+acaldwell1997@users.noreply.github.com>
2022-11-09 02:16:04 -0600

    Update script.js

--- CityWebServer/wwwroot/script.js
+++ CityWebServer/wwwroot/script.js
@@ -1,7 +1,34 @@
-function initializePopulationChart() {
+function initializeSplineChart() {
     var c = new Highcharts.Chart({
     chart: {
             renderTo: 'chart',
+            defaultSeriesType: 'spline',
+            events: { }
+        },
+        title: {
+            text: 'Statistics'
+        },
+        xAxis: {
+            type: 'datetime',
+            tickPixelInterval: 150
+        },
+        yAxis: {
+            minPadding: .02,
+            maxPadding: .02,
+            title: {
+                text: 'Value',
+                margin: 5
+            }
+        }
+    });
+    return c;
+}
+
+function initializePieChart() {
+    var c = new Highcharts.Chart({
+    chart: {
+            renderTo: 'chart2',
+            defaultSeriesType: 'pie',
             events: { }
         },
         title: {
@@ -15,7 +42,7 @@ function initializePopulationChart() {
             minPadding: 0.2,
             maxPadding: 0.2,
             title: {
-                text: 'Value',
+                text: 'Fullness Percentage',
                 margin: 80
             }
         }
@@ -24,7 +51,7 @@ function initializePopulationChart() {
 }
 
 
-function addOrUpdateSeries(theChart, seriesName, seriesType, value, valueName)
+function addOrUpdateSeries(theChart, seriesName, value, valueName)
 {
     var series;
     var matchFound = false;
@@ -43,68 +70,162 @@ function addOrUpdateSeries(theChart, seriesName, seriesType, value, valueName)
 
     if(!matchFound)
     {
-	if(seriesType = 'pie'){
-        var seriesOptions = {
-	    type: seriesType,
-            name: seriesName,
-            data: [{ name: valueName, y: value}],
-	    center: [80, 70],
-    	    size: 100,
-            showInLegend: false,
-    	    dataLabels: {
-      		enabled: false
-    	    }
-        };
-        series = theChart.addSeries(seriesOptions, false);
-	}else{
         //console.log("Adding series: " + seriesName);
         var seriesOptions = {
-	    type: seriesType,
+            id: seriesName,
             name: seriesName,
             data: [{ name: valueName, y: value}]
         };
         series = theChart.addSeries(seriesOptions, false);
-    }if (matchFound){
+    }
+    else
+    {
         var shift = series.data.length > 20;
         series.addPoint(value, true, shift);
     }
-console.log(series);
-}
 }
 
 function updateChart(vm, chart)
 {
     var updatedSeries = [];
     var districts = vm.Districts();
-    var chartSeries = ['Population','Density'];
-    var seriesName;
-    var value1;
-    var value2;
-    var seriesType;
 
     for(var i = 0; i < districts.length; i++)
     {
         var district = districts[i];
-        var districtName = district.DistrictName();
-	for(var j=0; j < chartSeries.length; j++){
-	if(chartSeries[j]='Population'){
-		seriesType = 'spline';
-		seriesName = districtName + " - Population";
-		value1 = district.TotalPopulationCount();
-		value2 = vm.Time();
-	} 
-if(chartSeries[j]='Density'){
-		seriesType = 'pie';
-		seriesName = districtName + " - Density";
-		value1 = district.TotalPopulationCount();
-		value2 = districtName;
-	}
-}
-        
-
-        addOrUpdateSeries(chart, seriesName, seriesType, value1, value2);
+	var districtName = district.DistrictName();
 	
-        updatedSeries.push(seriesName);
-     }
+        var seriesName = districtName + " - Population";
+	var seriesName2 = districtName + " - Jobs";
+	var seriesName3 = districtName + " - Density";
+        var population = doMath(district.CurrentHouseholds(), district.AvailableHouseholds());
+	var jobs = doMath(district.CurrentJobs(), district.AvailableJobs());
+	addOrUpdateSeries(chart, seriesName, population, vm.Time());
+	addOrUpdateSeries(chart, seriesName2, jobs, vm.Time());
+	updatedSeries.push(seriesName);
+    }
     chart.redraw();
 }
+
+function doMath(v1,v2){
+	var r = 0;
+
+	if(!isFinite((v1 / v2)*100) || isNaN((v1 / v2)*100)){
+		r = 0;
+	} else {
+		r = ((v1 / v2)*100).toFixed(0);
+		r = parseInt(r);
+}
+	return r;
+}
+
+function filterTable() {
+  // Declare variables
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("searchInput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("mainDataTable");
+  tr = table.getElementsByTagName("tr");
+
+  // Loop through all table rows, and hide those who don't match the search query
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
+
+function sortTable(n) {
+  var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+  table = document.getElementById("mainDataTable");
+  switching = true;
+  //Set the sorting direction to ascending:
+  dir = "asc"; 
+  /*Make a loop that will continue until
+  no switching has been done:*/
+  while (switching) {
+    //start by saying: no switching is done:
+    switching = false;
+    rows = table.rows;
+    /*Loop through all table rows (except the
+    first, which contains table headers):*/
+    for (i = 1; i < (rows.length - 1); i++) {
+      //start by saying there should be no switching:
+      shouldSwitch = false;
+      /*Get the two elements you want to compare,
+      one from current row and one from the next:*/
+      x = rows[i].getElementsByTagName("TD")[n];
+      y = rows[i + 1].getElementsByTagName("TD")[n];
+      /*check if the two rows should switch place,
+      based on the direction, asc or desc:*/
+      if (dir == "asc") {
+        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+          //if so, mark as a switch and break the loop:
+          shouldSwitch= true;
+          break;
+        }
+      } else if (dir == "desc") {
+        if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+          //if so, mark as a switch and break the loop:
+          shouldSwitch = true;
+          break;
+        }
+      }
+    }
+    if (shouldSwitch) {
+      /*If a switch has been marked, make the switch
+      and mark that a switch has been done:*/
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+      //Each time a switch is done, increase this count by 1:
+      switchcount ++;      
+    } else {
+      /*If no switching has been done AND the direction is "asc",
+      set the direction to "desc" and run the while loop again.*/
+      if (switchcount == 0 && dir == "asc") {
+        dir = "desc";
+        switching = true;
+      }
+    }
+  }
+}
+
+    var endpoint = "./CityInfo";
+    var viewModel;
+    var chart;
+    var chart2;
+    var initialized = false;
+    var lastDate;
+    let history = [];
+    
+    
+    // Update the data once every second.
+    window.setInterval(function ()
+    {
+        $.getJSON(endpoint, function(data) {
+            if (initialized) {
+                ko.mapping.fromJS(data, viewModel);
+                if (lastDate !== viewModel.Time()) {
+                    updateChart(viewModel, chart);
+                    lastDate = viewModel.Time();
+                } else {
+                    console.log("Same Date: " + lastDate);
+                }
+            } else {
+                initialized = true;
+                viewModel = ko.mapping.fromJS(data);
+                ko.applyBindings(viewModel);
+                chart = initializeSplineChart(viewModel);
+            }
+        });
+    }, 2000); // Every two seconds.
+
+	function appendHistoryTable(){
+		console.log(history)
+	}
